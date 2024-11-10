60ecbfb83375b92d3c5aa8d6bf51663441ac27de
Cameron King <35708477+cameronking4@users.noreply.github.com>
2024-11-10 03:37:16 -0500

    Add shadcn components + ai chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ChatSupport from "@/components/ui/chat/chat-support";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ChatSupport />
       </body>
     </html>
   );
